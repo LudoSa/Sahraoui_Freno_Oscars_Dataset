cbd7c17dcf29516d1b0c0b4c08296b5bf83b69cd
GabrielleFreno01 <freno.gabrielle@gmail.com>
2021-11-01 21:12:28 +0100

    Exception file not found User input for queries

--- src/main/Launcher/LauncherTest.scala
+++ src/main/Launcher/LauncherTest.scala
@@ -1,7 +1,9 @@
 import com.github.tototoshi.csv.{CSVReader, CSVWriter}
 
-import java.io.File
+import java.io.{File, FileNotFoundException, IOException}
 import java.util.Date
+import scala.io.StdIn.readLine
+import scala.util.{Failure, Success}
 
 
 
@@ -20,7 +22,7 @@ object LauncherTest {
 
     def LoadDataFromCSV(path : String): List[List[String]] = {
 
-      CSVReader.open(new File("C:\\Users\\ludov\\IdeaProjects\\OscarsDataset.csv")).toStream.toList.drop(1)
+      CSVReader.open(new File("C:\\Users\\Gab\\Desktop\\Scala\\Projet\\OscarsDataset.csv")).toStream.toList.drop(1)
 
 
     }
@@ -55,7 +57,7 @@ object LauncherTest {
 
   case class ProcessingServices(){
 
-    val writer = CSVWriter.open(copyOriginalCSV("C:\\Users\\ludov\\IdeaProjects\\OscarsDataset.csv"))
+    val writer = CSVWriter.open(copyOriginalCSV("C:\\Users\\Gab\\Desktop\\Scala\\Projet\\OscarsDataset.csv"))
 
     def test() = {
 
@@ -79,15 +81,39 @@ object LauncherTest {
   case class DataExplorationServices(){
 
 
-    def AllFilmsOfAYear(year : String, films : List[Film]) : List[String] = {
+    def AllFilmsOfAYear(films : List[Film])  = {
 
-      films.filter(_.releaseDate.equals(year)).map(x => x.name)
+      print("Enter a year between 1927 and 2021 : ")
+      val year = readLine()
+
+      val filmsByYear = films.filter(_.releaseDate.equals(year)).map(x => x.name)
+
+      if(filmsByYear.length > 0){
+        filmsByYear
+      }
+      else{
+        println(s"No film found for year $year")
+      }
+
 
     }
 
-    def AllFilmsTitleContainsWord(word : String, films : List[Film]) : List[String] = {
+    def AllFilmsTitleContainsWord(films : List[Film])  = {
 
-      films.filter(_.name.contains(word)).map(x => x.name)
+      print("Search a film's title that contain this word : ")
+      val word = readLine()
+
+      val filmsWithWord =  films.filter(_.name.contains(word)).map(x => x.name)
+
+
+      if(filmsWithWord.length > 0){
+        filmsWithWord
+      }
+      else{
+        println(s"No film's title contains the word $filmsWithWord")
+      }
+
+
 
     }
 
@@ -95,13 +121,22 @@ object LauncherTest {
 
       films.filter(_.award.equals("Winner")).map(x => x.name)
 
-
     }
 
-    def AllFilmsOfACompany(company : String, films : List[Film]) : List[String] = {
+    def AllFilmsOfACompany(films : List[Film])  = {
 
-      films.filter(_.company.equals(company)).map(x => x.name)
+      print("Search a all the film of a company : ")
+      val company = readLine()
+1945
+      val filmsByCompany =  films.filter(_.name.contains(company)).map(x => x.name)
 
+
+      if(filmsByCompany.length > 0){
+        filmsByCompany
+      }
+      else{
+        println(s"No film's found for the company $company")
+      }
 
     }
 
@@ -122,55 +157,68 @@ object LauncherTest {
 
     val p = new ProcessingServices()
 
-    val filmsList = l.getFilmsFromCSV(l.LoadDataFromCSV("C:\\Users\\ludov\\IdeaProjects\\OscarsDataset.csv"))
+    val filmsList = l.getFilmsFromCSV(l.LoadDataFromCSV("C:\\Users\\Gab\\Desktop\\Scala\\Projet\\OscarsDataset.csv"))
 
-    //Queries
+    try {
+      //Queries
 
-    //Query 1
-    println("-------------------------------------------------------------------------------------------------------")
-    println("Query 1")
-    println(d.AllFilmsOfAYear("1927", filmsList))
-    println("-------------------------------------------------------------------------------------------------------")
+      //Query 1
+      println("-------------------------------------------------------------------------------------------------------")
+      println("Query 1")
+      println(d.AllFilmsOfAYear(filmsList))
+      println("-------------------------------------------------------------------------------------------------------")
 
-    //Query 2
-    println("Query 2")
-    println(d.AllFilmsTitleContainsWord("The", filmsList))
-    println("-------------------------------------------------------------------------------------------------------")
-    //Query 3
-    println("Query 3")
-    println(d.AllWinnerFilms(filmsList))
-    println("-------------------------------------------------------------------------------------------------------")
+      //Query 2
+      println("Query 2")
+      println(d.AllFilmsTitleContainsWord(filmsList))
+      println("-------------------------------------------------------------------------------------------------------")
+      //Query 3
+      println("Query 3")
+      println(d.AllWinnerFilms(filmsList))
+      println("-------------------------------------------------------------------------------------------------------")
 
-    //Query 4
-    println("Query 4")
-    println(d.AllFilmsOfACompany("Warner Bros.", filmsList))
-    println("-------------------------------------------------------------------------------------------------------")
+      //Query 4
+      println("Query 4")
+      println(d.AllFilmsOfACompany(filmsList))
+      println("-------------------------------------------------------------------------------------------------------")
 
-    //Query 5
-    println("Query 5")
-    println(d.AllCertifiedFreshFilms(filmsList))
-    println("-------------------------------------------------------------------------------------------------------")
-
-
-    //Processing services
-
-    //PS 1
-    println("PS1 5")
-    p.test()
-    println("-------------------------------------------------------------------------------------------------------")
-
-    //PS 2
+      //Query 5
+      println("Query 5")
+      println(d.AllCertifiedFreshFilms(filmsList))
+      println("-------------------------------------------------------------------------------------------------------")
 
 
-    //PS 3
+      //Processing services
+
+      //PS 1
+      println("PS1 5")
+      p.test()
+      println("-------------------------------------------------------------------------------------------------------")
+
+      //PS 2
 
 
-
-    //PS 4
+      //PS 3
 
 
 
-    //PS 5
+      //PS 4
+
+
+
+      //PS 5
+
+
+    } catch {
+      case ex: FileNotFoundException => {
+        println("File not found")
+      }
+      case ex: IOException => {
+        println("IO Exception")
+      }
+    }
+
+
 
 
   }
